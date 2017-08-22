29c2896f58dc3b098911093115acd7f4e9c320ca
am7 <aayushmishra777@gmail.com>
2017-08-22 16:40:47 +0530

    Factorial checks negative numbers

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n<0)
+	{
+		cout<<"Enter a non-negative integer: ";
+		cin>>n;
+		return factorial(n);
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }

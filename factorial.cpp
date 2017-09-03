72dbabba799c307c1332631414a8fc201ad68bbf
rajanishWanderer <rajneeshkumar270497@gmail.com>
2017-09-03 08:25:23 +0530

    Added check for negative numbers and returned -1 for the same

--- factorial.cpp
+++ factorial.cpp
@@ -3,13 +3,9 @@
 #include <cstdio>
 using namespace std;
 int factorial(int n){
-	if(n<0){
-		printf("Enter a non-negative integer: ");
-		cin>>n;
-		return factorial(n);
-	}
-    if(n!=1){
-	return(n * factorial(n-1));
-    }
+	if(n<0)	return -1;
+	
+    if(n!=1) return(n * factorial(n-1));
+    
     else return 1;
 }
